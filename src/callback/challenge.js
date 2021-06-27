dd1765df444020631a4d3a7c828a4368f0ec9665
Jorge Trad <jetradl@gmail.com>
2021-06-26 21:55:23 -0500

    Multiple requests to an API with callbacks

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,11 +1,17 @@
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
+let API = 'https://rickandmortyapi.com/api/character/';
+
+
+
 function fetchData(url_api, callback){
     let xhttp = new XMLHttpRequest();
     xhttp.open('GET', url_api, true);
+    
     xhttp.onreadystatechange = function (event) {
-        if(xhhtp.onreadystatechange === 4) {
-            if(xhhtp.status === 200){
+        
+        if(xhttp.readyState === 4) {
+            if(xhttp.status === 200){
                 callback(null, JSON.parse(xhttp.responseText));
             } else {
                 const error = new Error('Error ' + url_api);
@@ -13,5 +19,18 @@ function fetchData(url_api, callback){
             }
         }
     }
-    xhhtp.send();
+    xhttp.send();
 }
+
+fetchData(API, function(error1, data1){
+    if(error1) return console.error(error1);
+    fetchData(API + data1.results[0].id, function(error2, data2){
+        if(error2) return console.error(error2);
+        fetchData(data2.origin.url, function(error3, data3) {
+            if(error3) return console.error(error3);
+            console.log(data1.info.count);
+            console.log(data2.name);
+            console.log(data3.dimension);
+        });
+    });
+});
